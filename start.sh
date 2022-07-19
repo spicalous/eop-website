7823297125f82b3bb00c28882fe86c3dc247031d
spicalous <benlertlumprasertkul@gmail.com>
2022-07-20 00:33:57 +0100

    use bootstrap; use collapsiable nav; added mailing list; added fb group to front page;

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 #1. `--base-url /eop` is used so that links are correct when built with Github pages
 #2. `--config _config.yml,_config_development.yml` override any prod config with dev config (e.g. analytics)
 
-jekyll serve --baseurl /eop --config _config.yml,_config_development.yml
+bundle exec jekyll serve --baseurl /eop --config _config.yml,_config_development.yml
